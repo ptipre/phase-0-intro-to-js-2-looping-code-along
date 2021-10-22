36f2a8149a2ec019af570cf4df9e5bbc34d6e3ff
Prajay Tipre <prajaytipre@gmail.com>
2021-10-22 13:07:44 -0400

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,37 @@
 // Code your solutions in this file
+
+// for (let age = 30; age < 40; age++) {
+//     console.log(`I'm ${age} years old. Happy birthday to me!`);
+// }
+
+// const gifts = ['teddy bear', 'drone', 'doll']
+
+// function  wrapGifts(gifts) {
+//     for (let i = 0; i < gifts.length; i++) {
+//         console.log(`Wrapped ${gifts[i]} and added a bow!`);
+//         debugger;
+//     }
+
+//     return gifts;
+// }
+
+// wrapGifts(gifts)
+
+const messages = []
+
+const names = ['Lisa', 'Kaitlin', 'Jan']
+
+function writeCards(arr, event) {
+    for (let i = 0; i < arr.length; i++) {
+        messages.push(`Thank you, ${names[i]}, for the wonderful ${event} gift!`)
+    }
+
+    return messages
+}
+
+function countDown(number) {
+    while (number >=0) {
+        console.log(number)
+        number--
+    }
+}
